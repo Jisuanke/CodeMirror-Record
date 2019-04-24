62c92f2bfbefb5fd04f983869ec712de90da8969
Haoran Yu <haoran_yu@hotmail.com>
2019-04-24 19:30:43 +0800

    Support input and delete compress in a single line. #1 #2

--- src/CodeRecord.js
+++ src/CodeRecord.js
@@ -21,7 +21,7 @@ class CodeRecord {
   printOperations() {
     console.log(this.operations)
     this.compressOperations()
-    console.log(this.compressedOperations)
+    // console.log(this.compressedOperations)
   }
 
   getOperationRelativeTime() {
@@ -52,9 +52,11 @@ class CodeRecord {
 
   changesListener(editor, changes) {
     this.operations.changes.push({
-      t: this.getOperationRelativeTime(), // time
-      d: this.getLastChangePause(), // duration
-      o: changes
+      startTime: this.getOperationRelativeTime(), // time
+      endTime: this.getOperationRelativeTime(), // time
+      beforeDuration: this.getLastChangePause(), // duration
+      o: changes,
+      combo: 1 // combo count
     })
   }
 
@@ -62,12 +64,118 @@ class CodeRecord {
     this.operations.cursorActivities.push({
       t: this.getOperationRelativeTime(), // time
       d: this.getLastCursorActivityPause(), // duration
-      o: editor.listSelections()
+      o: editor.listSelections(),
+      c: 1 // combo count
     })
   }
 
   compressOperations() {
-    this.compressedOperations = this.operations
+    let changes = this.operations.changes
+    changes = this.compressContinuousInput(changes)
+    changes = this.compressContinuousDelete(changes)
+    console.log(changes)
+  }
+
+  isContinueInput(firstChange, secondChange) {
+    if (firstChange.o.length !== secondChange.o.length) {
+      return false;
+    } else if (secondChange.beforeDuration >= this.acceptableMinDelay) {
+      return false;
+    } else {
+      for (let i = 0; i < secondChange.o.length; i++) {
+        if (secondChange.o[i].from.line !== secondChange.o[i].to.line ||
+            firstChange.o[i].from.line !== firstChange.o[i].to.line ||
+            secondChange.o[i].from.ch !== secondChange.o[i].to.ch ||
+            firstChange.o[i].from.ch !== firstChange.o[i].to.ch) {
+          return false;
+        } else if (firstChange.o[i].from.ch + 1 !== secondChange.o[i].from.ch) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  compressContinuousInput(changes) {
+    let newChanges = []
+    while(changes.length > 0) {
+      let change = changes.pop() // 拿出最新的一条
+      if (change.o[0].origin === '+input') {
+        while(changes.length > 1) {
+          let lastChange = changes.pop()
+          if (lastChange.o[0].origin === '+input' &&
+          this.isContinueInput(lastChange, change)) {
+            change.startTime = lastChange.startTime
+            change.beforeDuration = lastChange.beforeDuration
+            change.combo += 1
+            for (let i = 0; i < change.o.length; i++) {
+              change.o[i].from = lastChange.o[i].from
+              change.o[i].to = lastChange.o[i].to
+              change.o[i].text = lastChange.o[i].text.concat(change.o[i].text)
+            }
+          } else {
+            changes.push(lastChange)
+            break;
+          }
+        }
+        newChanges.unshift(change)
+      } else {
+        newChanges.unshift(change)
+      }
+    }
+    return newChanges
+  }
+
+
+  isContinueDelete(firstChange, secondChange) {
+    if (firstChange.o.length !== secondChange.o.length) {
+      return false;
+    } else if (secondChange.beforeDuration >= this.acceptableMinDelay) {
+      return false;
+    } else {
+      for (let i = 0; i < secondChange.o.length; i++) {
+        if (secondChange.o[i].from.line !== secondChange.o[i].to.line ||
+            firstChange.o[i].from.line !== firstChange.o[i].to.line) {
+          return false;
+        } else if (firstChange.o[i].from.ch !== secondChange.o[i].to.ch) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  compressContinuousDelete(changes) {
+    let newChanges = []
+    while(changes.length > 0) {
+      let change = changes.pop() // 拿出最新的一条
+      console.log('change', change.o[0].from, change.o[0].to)
+      if (change.o[0].origin === '+delete') {
+        while(changes.length > 1) {
+          let lastChange = changes.pop()
+          console.log('judge:', lastChange.o[0].from, lastChange.o[0].to, change.o[0].from, change.o[0].to)
+          if (lastChange.o[0].origin === '+delete' &&
+          this.isContinueDelete(lastChange, change)) {
+            change.startTime = lastChange.startTime
+            change.beforeDuration = lastChange.beforeDuration
+            change.combo += 1
+            for (let i = 0; i < change.o.length; i++) {
+              change.o[i].to = lastChange.o[i].to
+              change.o[i].removed = change.o[i].removed.concat(lastChange.o[i].removed)
+            }
+          } else {
+            changes.push(lastChange)
+            break;
+          }
+        }
+        newChanges.unshift(change)
+      } else {
+        newChanges.unshift(change)
+      }
+    }
+    return newChanges
   }
 }
 module.exports = CodeRecord
