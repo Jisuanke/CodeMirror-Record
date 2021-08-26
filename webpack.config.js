3bd8d1c7c12b1dda2a80f3bf72c2235190f4358d
Haoran Yu <haoran_yu@hotmail.com>
2021-08-26 18:37:04 +0800

    Update dependencies and release 1.1.1.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const moduleVal = {
   rules: [
-    {
-      enforce: 'pre',
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
-    },
     {
       test: /\.js$/,
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }
-  ]
-}
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
+  ],
+};
+
+const eslintOptions = {
+  extensions: [`js`, `jsx`],
+  exclude: [
+    `/node_modules/`,
+  ],
+};
 
 const config = [
   {
     entry: './src/index.js',
+    plugins: [new ESLintPlugin(eslintOptions)],
     output: {
       libraryTarget: 'umd',
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'dist'),
     },
-    module: moduleVal
+    module: moduleVal,
   },
   {
     entry: './demo/index.js',
+    plugins: [new ESLintPlugin(eslintOptions)],
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'demo')
+      path: path.resolve(__dirname, 'demo'),
     },
-    module: moduleVal
+    module: moduleVal,
   },
 ];
 
